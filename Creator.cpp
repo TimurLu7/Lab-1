cf46e576a99c2e13a37a8b1351b279f918870e31
Timur <timurlu7@mail.ru>
2025-09-19 22:36:21 +0300

    Creator

---
+++ Creator.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+struct employee {
+    int num;        
+    char name[10];  
+    double hours;   
+};
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        cout << "Incorrect input" << endl;
+        return 1;
+    }
+
+    const char* filename = argv[1];
+    int count = std::stoi(argv[2]);
+
+    ofstream out(filename, ios::binary);
+    if (!out) {
+        cout << "Can't open file" << endl;
+        return 1;
+    }
+    employee e;
+    for (int i = 0; i < count; i++) 
+    {
+        cout << "Введите данные сотрудника №" << (i + 1) << endl;
+        cout << "Номер: "; 
+        cin >> emp.num;
+        cout << "Имя: ";
+        cin >> emp.name;
+        cout << "Часы: ";
+        cin >> emp.hours;
+        out.write((char*)&emp, sizeof(employee));
+    }
+    out.close();
+    return 0;
+}
