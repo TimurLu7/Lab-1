cab38b2f9f5b5b76667add528df00919b3e70e58
Timur <timurlu7@mail.ru>
2025-09-19 23:02:52 +0300

    Reporter

---
+++ Reporter.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <windows.h>
+using namespace std;
+
+struct employee {
+    int num;
+    char name[10];
+    double hours;
+};
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        cout << "Incorrect input" << endl;
+        return 1;
+    }
+
+    const char* binfile = argv[1];
+    const char* textfile = argv[2];
+    double payment = atof(argv[3]);
+
+    ifstream in(binfile, ios::binary);
+    ofstream out(textfile);
+
+    if (!in || !out) {
+        cout << "Can't open files!" << endl;
+        return 1;
+    }
+
+    out << "Отчет по файлу: " << binfile << endl;
+    out << "Номер\tИмя\tЧасы\tЗарплата\n";
+
+    employee e;
+    while (in.read((char*)&e, sizeof(employee))) {
+        double pay = e.hours * payment;
+        out << e.num << "\t" << e.name << "\t"
+            << e.hours << "\t" << fixed << setprecision(2) << pay << "\n";
+    }
+
+    in.close();
+    out.close();
+    return 0;
+}
